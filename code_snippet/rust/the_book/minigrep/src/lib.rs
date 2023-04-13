dc14380cf96ddbcc899096cbeb09286118620e10
Ham <metalalive2008@gmail.com>
2023-04-13 12:17:49 +0800

    Update lib.rs

--- code_snippet/rust/the_book/minigrep/src/lib.rs
+++ code_snippet/rust/the_book/minigrep/src/lib.rs
@@ -1,5 +1,6 @@
 use std::error::Error as StdError;
 use std::{env, fs};
+use std::io::{self, BufReader, BufRead};
 
 pub struct Config {
     pub query: String,
@@ -8,67 +9,80 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build (args:&[String]) -> Result<Self, & 'static str> {
+    // the first argument can be any type which implements `Iterator` trait
+    pub fn build (mut args :impl Iterator<Item=String>) -> Result<Self, & 'static str>
+    {
         // set static lifetime to error string literal, make it
         // available as long as program runs
-        let mut errmsg:Option<&str> = None;
-        if args.len() < 3 {
-            errmsg = Some("not enough arguments");
-        } else if args[1].len() == 0 {
-            errmsg = Some("empty query string");
-        } else if args[2].len() == 0 {
-            errmsg = Some("empty file path for search");
-        }
-        match errmsg {
-            Some(detail) => Err(detail),
-            None => {
-                let _qry   = args[1].clone();
-                let _fpath = args[2].clone();
-                let  ign_ks = env::var("IGNORE_CASE").is_ok();
-                Ok(Self{query:_qry, file_path:_fpath,
-                    ignore_case:ign_ks, })
-            },
-        } // return based on the error message
+        args.next(); // discard, it should be the path to executable program
+        let _qry   : String = match args.next() {
+            Some(s) => if s.len() == 0 {return Err("empty query string");} else {s},
+            None => return Err("not enough arguments"),
+        };
+        let _fpath : String = match args.next() {
+            Some(s) => if s.len() == 0 {return Err("empty file path for search");} else {s},
+            None => return Err("not enough arguments"),
+        };
+        let  ign_ks = env::var("IGNORE_CASE").is_ok();
+        let _qry : String = if ign_ks {_qry.to_lowercase()} else {_qry};
+        Ok(Self{query:_qry, file_path:_fpath, ignore_case:ign_ks, })
+        // return based on the error message
     }
 } // end of impl config
 
-fn search<'a>(fulltxt: & 'a str, keyword:& str, ign_ks:bool) -> Vec<& 'a str>
-{ // the lifetime of each result must be the same as fcontent
-    let mut out:Vec<&str> = vec![];
-    if fulltxt.is_empty() || keyword.is_empty() {
+fn chk_one_line (line:& str, keyword:& str, l_ign_ks:bool) -> bool
+{
+    if line.is_empty() || keyword.is_empty() {
+        false
     } else {
-        // to-lowercase() will generate new value and allocate new space
-        // it is safe to claim the varaible for appropriate lifetime
-        let kw:String = keyword.to_lowercase();
-        let kw:&str = & kw.as_str();
-        for line in fulltxt.lines() {
-            let pattern_exists:bool = 
-                if ign_ks {
-                    let l:String = line.to_lowercase();
-                    let l:&str = &l.as_str();
-                    l.contains(kw)
-                } else {line.contains(keyword)};
-            if pattern_exists {
-                out.push(line);
-            }
-        } // end of loop
+        if l_ign_ks {
+            let l:String = line.to_lowercase();
+            let l:&str = &l.as_str();
+            l.contains(keyword)
+        } else {
+            line.contains(keyword)
+        }
     }
-    out
-} // end of search
+}
+
+
+fn search_and_print (_file:fs::File, cfg:&Config,
+                     _linechk:fn(&str, &str, bool) -> bool ) -> u32
+{ // read portion of file instead of entire content
+    let rdbuf = BufReader::new(_file);
+    // `BufReader::lines()`
+    // - return an iterator with items of type `Result<String, io::Error>`
+    // - move ownership of the receiver (the BufReader instance)
+    let all_lines = rdbuf.lines();
+    // the closure has to capture struct fields in the same scope,
+    // so it cannot be converted to a function
+    let f = |line:&Result<String, io::Error>| -> bool {
+        match line {
+            Ok(s) => _linechk( &s.as_str(),
+                &cfg.query.as_str(), cfg.ignore_case),
+            Err(_) => false,
+        }
+    };
+    let mut num:u32 = 0;
+    for line in all_lines.filter(f) {
+        println!("found line : {:?}", line);
+        num += 1;
+    }
+    num
+} // end of search_and_print
 
 pub fn run(cfg:&Config) -> Result<(), Box<dyn StdError>>
-{ // TODO, read portion of file instead of entire content
-    let fcontent = fs::read_to_string(cfg.file_path.as_str()) ?;
-    for line in search(&fcontent, &cfg.query, cfg.ignore_case) {
-        println!("found line : {}", line);
-    }
+{
+    let f = fs::File::open(cfg.file_path.as_str()) ?;
+    let num = search_and_print(f, cfg, chk_one_line);
+    println!("num of lines found : {num}");
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use super::Config;
-    use crate::search;
+    use crate::chk_one_line;
 
     #[test]
     fn build_cfg_ok() {
@@ -77,9 +91,11 @@ mod tests {
             "fabric".to_string(),
             "/path/to/file".to_string()
         ];
-        let actual_cfg = Config::build(&t_args);
+        let actual_cfg = Config::build(t_args.into_iter());
         assert_eq!(actual_cfg.is_ok(), true);
-        assert_eq!(actual_cfg.unwrap().file_path, "/path/to/file");
+        let actual_cfg = actual_cfg.unwrap();
+        assert_eq!(actual_cfg.file_path, "/path/to/file");
+        assert_eq!(actual_cfg.query, "fabric");
     }
 
     #[test]
@@ -88,7 +104,7 @@ mod tests {
             "/path/to/myprogram".to_string(),
             "fabric".to_string(),
         ];
-        let actual_cfg = Config::build(&t_args);
+        let actual_cfg = Config::build(t_args.into_iter());
         assert_eq!(actual_cfg.is_err(), true);
     }
 
@@ -99,32 +115,30 @@ mod tests {
             "fabric".to_string(),
             "".to_string()
         ];
-        let actual_cfg = Config::build(&t_args);
+        let actual_cfg = Config::build(t_args.into_iter());
         assert_eq!(actual_cfg.is_err(), true);
     }
 
     #[test]
     fn saerch_missing_arg() {
-        let result:Vec<&str> = search(&"", &"", false);
-        assert_eq!(result.len(), 0);
-        let result:Vec<&str> = search(&"abcde", &"", false);
-        assert_eq!(result.len(), 0);
-        let result:Vec<&str> = search(&"", &"abcde", true);
-        assert_eq!(result.len(), 0);
+        let result = chk_one_line(&"", &"", false);
+        assert_eq!(result, false);
+        let result = chk_one_line(&"abcde", &"", false);
+        assert_eq!(result, false);
+        let result = chk_one_line(&"", &"abcde", true);
+        assert_eq!(result, false);
     }
 
     #[test]
     fn saerch_found() {
-        let result:Vec<&str> = search(&"temp probe\nban prohibit\nclosure\n", &"pro", false);
-        assert_eq!(result.len(), 2);
-        let result:Vec<&str> = search(&"one-liner\nsolution\nwhich", &"lut", false);
-        assert_eq!(result.len(), 1);
-        let result:Vec<&str> = search(&"eagle-fly\nfree-secure\nHoseClimb\nbasecamp\n", &"sec", false);
-        assert_eq!(result.len(), 2);
-        let result:Vec<&str> = search(&"eagle-fly\nfree-secure\nHoseClimb\nbasecamp\n", &"sec", true);
-        assert_eq!(result.len(), 3);
-        let result:Vec<&str> = search(&"sure\n5ur3\nSuRe\nbingo\nsUrE\n", &"URE", true);
-        assert_eq!(result.len(), 3);
+        let result = chk_one_line(&"temp probe;closure", &"pro", false);
+        assert_eq!(result, true);
+        let result = chk_one_line(&"one-soLution", &"lut", false);
+        assert_eq!(result, false);
+        let result = chk_one_line(&"free-sEcUre\n", &"secu", true);
+        assert_eq!(result, true);
+        let result = chk_one_line(&"free-sEcUre\n", &"secu", false);
+        assert_eq!(result, false);
     }
 } // end of tests module
 
